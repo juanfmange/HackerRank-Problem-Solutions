1ed367a91d465c2efe9a53342b699a451dc12c5e
Juan Fco. Mange <juanfmange@gmail.com>
2023-09-29 14:06:10 -0600

    A Veryu Big Sum - solved

--- A_Very_Big_Sum.py
+++ A_Very_Big_Sum.py
@@ -1,35 +1,6 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-#
-# Complete the 'aVeryBigSum' function below.
-#
-# The function is expected to return a LONG_INTEGER.
-# The function accepts LONG_INTEGER_ARRAY ar as parameter.
-#
-
 def aVeryBigSum(ar):
-    # Write your code here
-    sum = 0
-    for i in range(len(ar)):
-        sum+=ar[i]
+    sum = 0 # Valor incial
+    for i in range(len(ar)): #bucle que recorre la longitud del arreglo
+        sum+=ar[i] #tomamos la longitud del arreglo y almacenamos en la variable sum
     return sum
-    
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input().strip())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = aVeryBigSum(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+print(aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]))
