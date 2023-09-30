29e8fbcec5cfc8bb68c2b00f90b8d160564be69f
Juan Fco. Mange <juanfmange@gmail.com>
2023-09-30 12:27:27 -0600

    Plus Minus - Solved

--- Plus_Minus.py
+++ Plus_Minus.py
@@ -1,36 +1,17 @@
-arr = [1,1,0,-1,-1]
-p = 0
-n = 0
-z = 0
-for i in arr:
-    
-    if i == 0:
-        print("zero")
-        z = z + 1
-    elif i >= 1:
-        print("positivo")
-        p =  p + 1
-    elif i < 0:
-        print("negativo")
-        n = n + 1
-    print(i)
-print(p,n,z)
-    
-print(p / len(arr))
-print("{:.6f}".format(p / len(arr)))
-
 def plus_minus(arr):
+    p = 0
+    n = 0
+    z = 0
     for i in arr:
         if i == 0:
-            z =+ 1
+            z += 1
         elif i >= 1:
-            p =+ 1
+            p += 1
         elif i < 0:
-            n =+ 1
-    positive = "{:.6f}".format(p / len(arr))
-    negative = "{:.6f}".format(n / len(arr))
-    zero = "{:.6f}".format(z / len(arr))
-    return positive, negative, zero
+            n += 1
+    print("{:.6f}".format(p / len(arr)))
+    print("{:.6f}".format(n / len(arr)))
+    print("{:.6f}".format(z / len(arr)))
 
-plus = plus_minus(arr)
-print(plus)
+arr = [1, 1, 0, -1, -1]
+plus_minus(arr)
